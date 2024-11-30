5f79f0fe6c95a44f0007338bdb8a0dfaf005e425
Kevin BJ <kevin.b.jonsson@hotmail.com>
2024-11-30 12:00:29 +0100

    Add project files.

---
+++ Lab4/GitHubProjektRepo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+    public class GitHubProjektRepo
+    {
+        [JsonPropertyName("name")] public string Name { get; set; }
+        [JsonPropertyName("description")] public string Description { get; set; }
+        [JsonPropertyName("html_url")] public string HtmlUrl { get; set; }
+        [JsonPropertyName("homepage")] public string Homepage { get; set; }
+        [JsonPropertyName("watchers")] public int Watchers { get; set; }
+        [JsonPropertyName("pushed_at")] public string PushedAt { get; set; }
+    }
+    public class ZipCodeInfo
+    {
+        [JsonPropertyName("places")] public List<Place> Places { get; set; }
+        [JsonPropertyName("post code")] public string PostCode { get; set; } //Visning av postnummer
+    }
+    public class Place
+    {
+        [JsonPropertyName("place name")] public string Places { get; set; }
+        [JsonPropertyName("state")] public string State { get; set; }
+        [JsonPropertyName("latitude")]  public string Latitude { get; set; }
+        [JsonPropertyName("longitude")]  public string Longitude { get; set; }
+    }
+}
---
+++ Lab4/Program.cs
@@ -0,0 +1,54 @@
+﻿using Lab4;
+using System;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+class Program
+{
+    private static readonly HttpClient client = new HttpClient(); //Github
+    static async Task Main(string[] args)
+    {
+        client.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("Lab4", "1.0")); //API krav (Github)
+        await GetDotNetRepositories(); //Hämtning av repositories, Github
+        await GetZipCodeInfo(); //Hämtning av repositories, Zippopotam
+    }
+    private static async Task GetDotNetRepositories()
+    {
+        var url = "https://api.github.com/orgs/dotnet/repos";
+        var response = await client.GetStringAsync(url);
+        var repositories = JsonSerializer.Deserialize<List<GitHubProjektRepo>>(response);
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        Console.WriteLine("Repositories under .Net Foundation:");
+        Console.ResetColor();
+        foreach (var repo in repositories)
+        {
+            Console.WriteLine($"\nName: {repo.Name}"
+                + $"\nHomepage: {repo.Homepage}"
+                + $"\nGitHub: {repo.HtmlUrl}"
+                + $"\nDescription: {repo.Description}"
+                + $"\nWatchers: {repo.Watchers}"
+                + $"\nLast push: {repo.PushedAt}\n");
+        }
+    }
+    private static async Task GetZipCodeInfo()
+    {
+        var url = "https://api.zippopotam.us/us/07645";
+        var response = await client.GetStringAsync(url);
+        var zipCodeInfo = JsonSerializer.Deserialize<ZipCodeInfo>(response);
+        if (zipCodeInfo != null && zipCodeInfo.Places.Count > 0)
+        {
+            var place = zipCodeInfo.Places[0];
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Here is the extra detail:"); //Grön text för de önskade extra utskrivt från Zippopotam
+            Console.ResetColor();
+            Console.WriteLine($"ZIP Code: {zipCodeInfo.PostCode}"
+                + $"\nPlace: {place.Places}"
+                + $"\nState: {place.State}"
+                + $"\nLatitude: {place.Latitude}"
+                + $"\nLongitude: {place.Longitude}");
+        }
+        else { Console.WriteLine("No data found for the ZIP code."); }
+    }
+}
